c064c659761176b8fbaee23b174546473ff2b22f
mattrices <31460714+mattrices@users.noreply.github.com>
2019-10-02 05:32:10 -0400

    Add files via upload
    copy codepen

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
+"use strict";
+
 $(window).scroll(function () {
-  const
-  a = $(this).scrollTop(),
-  b = 400,
-  t = window.pageYOffset;
-
+  var a = $(this).scrollTop(),
+      b = 400,
+      t = window.pageYOffset;
   $(".parallax").css('transform', 'translate(-' + a / 1.6 + 'px, -' + 1 - a / b + 'px)');
-
   $(".one").css('bottom', -(t * 0.3) + 'px');
   $(".two").css('bottom', -(t * 0.4) + 'px');
   $(".three").css('bottom', -(t * 0.5) + 'px');
   $(".four").css('bottom', -(t * 0.6) + 'px');
   $(".five").css('bottom', -(t * 0.7) + 'px');
 });
-
-$("header").on('mousemove', e => {
-  const cx = $(window).width() / 2,
-  cy = $(window).height() / 2,
-  x = (cx - e.pageX) / cx * 2,
-  y = (cy - e.pageY) / cy * 2;
-  $(".two").css('transform', `translate(${x}px, ${y}px)`);
-  $(".four").css('transform', `translate(-${x}px, ${y}px)`);
-  $(".five").css('transform', `translate(${x}px, -${y}px)`);
+$("header").on('mousemove', function (e) {
+  var cx = $(window).width() / 2,
+      cy = $(window).height() / 2,
+      x = (cx - e.pageX) / cx * 2,
+      y = (cy - e.pageY) / cy * 2;
+  $(".two").css('transform', "translate(".concat(x, "px, ").concat(y, "px)"));
+  $(".four").css('transform', "translate(-".concat(x, "px, ").concat(y, "px)"));
+  $(".five").css('transform', "translate(".concat(x, "px, -").concat(y, "px)"));
 });
 
 (function () {
   var v = document.getElementsByClassName("youtube-player");
-  for (var n = 0; n < v.length; n++) {if (window.CP.shouldStopExecution(0)) break;
+
+  for (var n = 0; n < v.length; n++) {
     var p = document.createElement("div");
     p.innerHTML = labnolThumb(v[n].dataset.id);
     p.onclick = labnolIframe;
     v[n].appendChild(p);
-  }window.CP.exitedLoop(0);
+  }
 })();
 
 function labnolThumb(id) {
